5fc0f83abeb811651e4448413bb6edb83e995ed5
SamuelAdamson <adamson.samuel10@gmail.com>
2023-12-13 16:27:45 -0700

    File structure for refresh cloud function

---
+++ api/refresh/main.py
@@ -0,0 +1,10 @@
+# Samuel Adamson (giibbu)
+# Portfolio Page
+# Refresh -- Main
+
+""" Update Cloud Firestore with recent metrics """
+
+def refresh_main(request) -> None:
+    """ Refresh daily metrics in Firestore (Entry Point) """
+
+
---
+++ api/refresh/query.py
@@ -0,0 +1,13 @@
+# Samuel Adamson (giibbu)
+# Portfolio Page
+# Refresh -- Query
+
+""" Helper functions for querying cloud metrics """
+
+
+def query_github_metrics():
+    """ Todo """
+
+
+def query_leetcode_metrics():
+    """ Todo """
