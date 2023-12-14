499ee1f8179a2b06872797d1b615cd158baf5590
SamuelAdamson <adamson.samuel10@gmail.com>
2023-12-13 21:07:04 -0700

    Working on refresh for populating firestore

--- api/refresh/query.py
+++ api/refresh/query.py
@@ -4,10 +4,49 @@
 
 """ Helper functions for querying cloud metrics """
 
+import os
+import requests
+import logging
 
-def query_github_metrics():
+
+def get_github_metrics() -> list:
+    """ Get github commit metrics """
+
+    username = os.environ.get("GH_USERNAME")
+    token = os.environ.get("GH_TOKEN")
+
+    repos = _get_github_repos(username, token)
+
+
+
+
+def get_leetcode_metrics():
     """ Todo """
 
 
-def query_leetcode_metrics():
-    """ Todo """
+def _get_github_repos(username: str, token: str) -> list[str] | None:
+    """ Get a list of all repositories for a user """
+
+    api_url = f"https://api.github.com/user/repos"
+
+    res = requests.get(api_url, auth=(username, token))
+
+    if res.status_code == 200:
+        
+        data = res.json()
+        repos = []
+
+        for repo in data:
+
+            if "full_name" not in repo:
+                logging.error("Error! API schema has changed!")
+                return None
+            
+            repos.append(repo["full_name"])
+
+        return repos
+
+    logging.error("Error! GitHub API request failed!")
+    logging.error(f"{res.status_code} {res.text}")
+
+    return None
---
+++ scripts/load_environment_variables.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run with `source load_environment_variables.sh`
+
+# The purpose of this script is to load environment variables from a .env file
+# in development environments. In Python Google Cloud Functions, environment
+# variables are accessed using `os.environ.get()`. Thus, we are storing 
+# environment variables in the same way.
+
+load_env_file() {
+    env_file="$1"
+    if [ -f "$env_file" ]; then
+        while IFS= read -r line; do
+            export "$line"
+        done < "$env_file"
+        echo "Environment variables loaded from $env_file"
+    fi
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: source $0 <source_directory>"
+    exit 1
+fi
+
+dir="$1"
+
+# recursively search all directories
+for dir in $(find ${dir} -maxdepth 5 -type d -not -path **/.git*); do
+    if [ -f "${dir}/.env" ]; then
+        load_env_file "${dir}/.env"
+    fi
+done
