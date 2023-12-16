eb6fa33860e680987b19f35f5b05ea6cb9691c76
SamuelAdamson <adamson.samuel10@gmail.com>
2023-12-16 14:56:53 -0700

    Some updates and added default environment file for refresh

--- api/refresh/query.py
+++ api/refresh/query.py
@@ -8,7 +8,7 @@ import os
 import requests
 import logging
 from datetime import datetime, timedelta
-from time import sleep
+
 
 def get_github_contributions() -> tuple[int, dict]:
     """ Get github contributions in the last year grouped by month """
@@ -26,9 +26,22 @@ def get_github_contributions() -> tuple[int, dict]:
 def get_github_repos(max_len: int=12) -> list:
     """ Get github public repositories sorted by recency """
 
+    username = os.environ.get("GH_USERNAME")
+    token = os.environ.get("GH_TOKEN")
+
+    last_year = _get_last_year()
+
+    repos = _get_github_repos_since(
+        username,
+        token,
+        last_year
+    )
+
+    return repos
+
 
 def get_leetcode_metrics():
-    """ Todo """
+    """ Get number of solved hard, medium, and easy leetcode problems """
 
 
 
@@ -82,7 +95,10 @@ def _get_github_contributions_since(
                     continue
 
                 total += day["contributionCount"]
-                months[date.strftime("%Y-%m")] += day["contributionCount"]
+                year_month = date.strftime("%Y-%m")
+
+                if year_month in months:
+                    months[date.strftime("%Y-%m")] += day["contributionCount"]
 
         return total, months
 
@@ -100,7 +116,7 @@ def _get_github_repos_since(
     since: datetime
 ) -> list[str] | None:
     """ Get a list of all repositories for a user which have been updated
-    since a given date
+    since a given date, sorted by most recently updated
     """
 
     api_url = "https://api.github.com/user/repos"
@@ -108,6 +124,7 @@ def _get_github_repos_since(
     since_str = since.strftime("%Y-%m-%dT%H:%M:%S%Z")
     params = {
         "since": since_str,
+        "visibility": "public",
     }
 
     res = requests.get(api_url, auth=(username, token), params=params)
@@ -119,19 +136,27 @@ def _get_github_repos_since(
 
         for repo in data:
 
-            if "full_name" not in repo:
+            if "full_name" not in repo or "updated_at" not in repo:
 
                 logging.error(f"GitHub API schema has changed for {api_url}!")
                 return None
             
-            repos.append(repo["full_name"])
+            repos.append((repo["full_name"], repo["updated_at"]))
 
-        return repos
+        sorted_repos = sorted(repos, key=lambda r: r[1], reverse=True)
+        return [repo[0] for repo in sorted_repos]
 
     logging.error(f"GitHub API request failed for {api_url}!")
     logging.error(f"{res.status_code} {res.text}")
 
     return None
+
+
+def _get_leetcode_problem_count() -> dict[str, int] | None:
+    """ Get leetcode problems grouped by difficulty """
+
+    
+
 
 def _get_last_year() -> datetime:
     """ Get last year's datetime (start of day) """
@@ -162,4 +187,4 @@ def _get_month_dict(since: datetime) -> dict[str, 0]:
 
     return months
 
-get_github_contributions()
+get_github_repos()
