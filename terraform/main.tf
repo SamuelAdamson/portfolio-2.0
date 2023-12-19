7b44cc7e1c6d1582e58165e5952beeb6c0c6e6fa
SamuelAdamson <adamson.samuel10@gmail.com>
2023-12-19 13:05:42 -0700

    updated deletion policy for metrics db

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,10 @@
 
 # Firestore for api metrics
 resource "google_firestore_database" "metrics_db" {
-    project     = var.gcp_project_id
-    name        = var.gcp_firestore_id
-    location_id = "nam5"
-    type        = "FIRESTORE_NATIVE"
+    project                 = var.gcp_project_id
+    name                    = var.gcp_firestore_id
+    location_id             = "nam5"
+    type                    = "FIRESTORE_NATIVE"
+    delete_protection_state = "DELETE_PROTECTION_DISABLED"
+    deletion_policy         = "DELETE"
 }
